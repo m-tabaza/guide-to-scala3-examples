a07f065690adb758f9e5c9553ad3e2ae501d46d5
Tabzz98 <muhammadtabaza@gmail.com>
2019-10-15 16:57:25 +0300

    Added enum example.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -32,3 +32,56 @@ def h(v: A | Double | B | Boolean): String = v match {
   case b: B => s"Int value b: ${b.b}"
   case bool: Boolean => s"Boolean value bool: $bool"
 }
+
+
+enum WeekDay {
+  case Sunday, Monday, Tuesday, Wednesday, 
+       Thursday, Friday, Saturday
+}
+
+sealed trait VerboseLogicalExpression {
+  import VerboseLogicalExpression._
+  def eval: Boolean = this match {
+    case ConstFactor(c) => c
+    case NotFactor(c) => !c
+    case Term(l, Some(r)) => l.eval && r.eval
+    case Term(l, None) => l.eval
+    case Expr(l, Some(r)) => l.eval || r.eval
+    case Expr(l, None) => l.eval
+  }
+}
+object VerboseLogicalExpression {
+  sealed trait Factor extends VerboseLogicalExpression
+  case class ConstFactor(value: Boolean) extends Factor
+  case class NotFactor(value: Boolean) extends Factor
+  case class Term(left: Factor, right: Option[Factor]) 
+    extends VerboseLogicalExpression
+  case class Expr(left: Term, right: Option[Term]) 
+    extends VerboseLogicalExpression
+}
+
+enum LogicalExpression {
+  case ConstFactor(value: Boolean)
+  case NotFactor(value: Boolean)
+  case Term(
+    left: ConstFactor | NotFactor, 
+    right: Option[ConstFactor | NotFactor]
+  )
+  case Expr(left: Term, right: Option[Term])
+
+  def eval: Boolean = this match {
+    case ConstFactor(c) => c
+    case NotFactor(c) => !c
+    case Term(l, Some(r)) => l.eval && r.eval
+    case Term(l, None) => l.eval
+    case Expr(l, Some(r)) => l.eval || r.eval
+    case Expr(l, None) => l.eval
+  }
+}
+
+import LogicalExpression._
+
+val expr = new Expr(
+  new Term(new ConstFactor(true), Some(new NotFactor(false))), 
+  Some(new Term(new ConstFactor(false), None))
+)
